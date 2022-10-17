b7e99b7ff34871a9c70c572ea480a8480155cf87
Essien-Ubong <essienessienubong2@gmail.com>
2022-10-16 18:41:31 -0700

    Puts created

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
 #!/bin/bash
-gcc $CFILE -v "Programming is like building a multilingual puzzle
+#include<stdio.h>
+
+int main()
+
+{
+  char Mystr[] = "Programming is like building a multilingual puzzle";
+  puts(Mystr);
+  return 0;
+}
