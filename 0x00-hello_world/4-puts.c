f098bdf09b7c60620f9ee9da7c09e7980f56309f
Essien-Ubong <essienessienubong2@gmail.com>
2022-10-17 19:36:47 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - This is the main function
- * return - Assigned integer to be returned
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
-
-	puts(Mystr);
-	/* return - the value of 0 is assigned */
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
