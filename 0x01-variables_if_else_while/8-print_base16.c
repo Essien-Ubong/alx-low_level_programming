a873c476c968afa1633ec89459e7a1bc549e7337
Essien-Ubong <essienessienubong2@gmail.com>
2022-10-18 00:01:34 -0700

    Print hexa

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,7 +7,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
+{
 	int d;
 	char low;
 
@@ -15,7 +15,7 @@ int main(void)
 		putchar(d);
 	for (low = 'a'; low <= 'f'; low++)
 		putchar(low);
-		putchar('\n')
+	putchar('\n')
 
 	return (0);
 }
