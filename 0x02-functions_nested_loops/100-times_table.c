c269b23fb5228d57bd7c897905739e56ad472fc8
Essien-Ubong <essienessienubong2@gmail.com>
2022-10-19 22:35:09 -0700

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,26 +5,26 @@
  * @n : times table to use
  * Return:void
  */
+
 void print_times_table(int n)
 {
 	int a = 0, rep, b;
 
 	if (n < 0 || n > 15)
 		return;
+
 	while (a <= n)
 	{
 		for (b = 0; b <= n; b++)
 		{
 			rep = a * b;
 			if (b == 0)
-			{
 				_putchar('0' + rep);
-			}
 			else if (rep < 10)
 			{
 				_putchar(' ');
 				_putchar(' ');
-				_putchar('0' + rep);
+				_putchar('0' + rep)
 			}
 			else if (rep < 100)
 			{
@@ -44,7 +44,7 @@ void print_times_table(int n)
 				_putchar(' ');
 			}
 		}
-		_putchar('\n');
-		a++;
+			_putchar('\n');
+			a++;
 	}
 }
