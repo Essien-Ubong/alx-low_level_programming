c682ce195edb251db89e9308204dc14f7eef8478
Essien-Ubong <essienessienubong2@gmail.com>
2022-10-25 02:33:01 -0700

    Print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - printa an array of integers, followed by a new line
+ * @a: int type array pointer
+ * @n: int type integer
+ * Description: Numbers must be separated by comma, followed by a space
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; n--, i++)
+	{
+		printf("%d", a[i])
+			if (n > 0)
+			{
+				printf(", ")
+			}
+	}
+	printf("\n")
+}
+
+
