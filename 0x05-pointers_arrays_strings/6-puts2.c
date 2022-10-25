1bb6a79e396eb16b72de51dec89c998ca12226a4
Essien-Ubong <essienessienubong2@gmail.com>
2022-10-25 01:57:22 -0700

    Puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 
 /**
  * puts2 - prints a string, followed by a new line
- * @s: pointer to string to print
+ * @str: pointer to string to print
  * Return: void
  */
 void puts2(char *str)
@@ -11,11 +11,12 @@ void puts2(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (i % 2 ==0)
+		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
 		i++;
 	}
+
 	_putchar('\n');
 }
